21127347fd6e49e9275578ef1a9e011618265972
CLINIOI <igor230509@gmail.com>
2024-05-24 23:48:00 +0300

    12

--- GoBus/script/Sr.js
+++ GoBus/script/Sr.js
@@ -1,23 +1,3 @@
-// Инициализация индекса слайда
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Функция добавляет или убавляет число к индексу слайда (переход вперед/назад)
-function moveSlide(n) {
-  showSlides(slideIndex += n);
-}
-
-// Основная функция для отображения слайдов
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1} // Если n больше количества слайдов, возвращаемся к первому слайду
-  if (n < 1) {slideIndex = slides.length} // Если n меньше 1, переходим к последнему слайду
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; // Сначала скрываем все слайды
-  }
-  slides[slideIndex-1].style.display = "block"; // Показываем выбранный слайд
-}
 
 
 //поиск 
