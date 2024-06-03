68d9272cbb8f1a85cb5848f7f7825cc8bde38e2a
CLINIOI <igor230509@gmail.com>
2024-06-03 21:59:40 +0300

    21

--- GoBus/script/Sr.js
+++ GoBus/script/Sr.js
@@ -1,19 +1,19 @@
-
-//поиск 
+//поиск
 function startSearch() {
-    const inputA = document.getElementById('inputA').value;
-    const inputB = document.getElementById('inputB').value;
-    const searchParams = new URLSearchParams({ a: inputA, b: inputB });
+  const inputA = document.getElementById("inputA").value;
+  const inputB = document.getElementById("inputB").value;
+  const searchParams = new URLSearchParams({ a: inputA, b: inputB });
 
-    window.location.href = `search.html?${searchParams.toString()}`;
+  window.location.href = `search.html?${searchParams.toString()}`;
 }
 
 // JavaScript-код для поиска и отображения результатов
 function startSearch() {
-    const inputA = document.getElementById('inputA').value;
-    const inputB = document.getElementById('inputB').value;
-    const searchParams = new URLSearchParams({ a: inputA, b: inputB });
+  const inputA = document.getElementById("inputA").value;
+  const inputB = document.getElementById("inputB").value;
+  const searchParams = new URLSearchParams({ a: inputA, b: inputB });
 
-    window.location.href = `search.html?${searchParams.toString()}`;
+  window.location.href = `search.html?${searchParams.toString()}`;
 }
 
+AOS.init();
